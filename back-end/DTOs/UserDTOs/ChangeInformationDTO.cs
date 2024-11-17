3c3afc28e72fe944c630f678251f8f9375eb437a
Phuong Do Manh <phuong.dm225660@sis.hust.edu.vn>
2024-11-17 18:15:59 +0700

    add more attributes

--- back-end/DTOs/UserDTOs/ChangeInformationDTO.cs
+++ back-end/DTOs/UserDTOs/ChangeInformationDTO.cs
@@ -2,6 +2,8 @@ namespace back_end.DTOs.UserDTOs
 {
     public class ChangeInformationDTO
     {
-        public string Email { get; set; } = String.Empty;
+        public string? FullName { get; set; } = String.Empty;
+        public string? PhoneNumber { get; set; } = String.Empty;
+        public DateTime? DateOfBirth { get; set; } 
     }
 }
